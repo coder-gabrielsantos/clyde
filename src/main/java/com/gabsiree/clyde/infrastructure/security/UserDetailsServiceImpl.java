164ab0f83b9f99e48fc33bc9deac41dbb9f4095c
Gabriel Santos <coder.gabrielsantos@gmail.com>
2025-04-06 20:49:14 -0300

    feat: implement UserDetailsService with database support

---
+++ src/main/java/com/gabsiree/clyde/infrastructure/security/UserDetailsServiceImpl.java
@@ -0,0 +1,31 @@
+package com.gabsiree.clyde.infrastructure.security;
+
+import com.gabsiree.clyde.domain.model.User;
+import com.gabsiree.clyde.domain.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.*;
+import org.springframework.stereotype.Service;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + email));
+
+        return org.springframework.security.core.userdetails.User.builder()
+                .username(user.getEmail())
+                .password(user.getPassword())
+                .roles(user.getRole().name())
+                .build();
+    }
+}
