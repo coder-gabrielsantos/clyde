34772f265cf10ae9db02570c7a1768675f945cbc
Gabriel Santos <coder.gabrielsantos@gmail.com>
2025-04-06 14:51:46 -0300

    chore: estrutura inicial do projeto Clyde

---
+++ src/main/java/com/gabsiree/clyde/ClydeApplication.java
@@ -0,0 +1,13 @@
+package com.gabsiree.clyde;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClydeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClydeApplication.class, args);
+    }
+
+}
