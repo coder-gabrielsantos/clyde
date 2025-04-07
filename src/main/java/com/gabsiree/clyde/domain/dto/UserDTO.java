dec02925735205baeb244cbcc0f806eabfe57fd1
Gabriel Santos <coder.gabrielsantos@gmail.com>
2025-04-07 09:56:10 -0300

    Refactoring

--- src/main/java/com/gabsiree/clyde/domain/dto/UserDTO.java
+++ src/main/java/com/gabsiree/clyde/domain/dto/UserDTO.java
@@ -3,6 +3,7 @@ package com.gabsiree.clyde.domain.dto;
 import com.gabsiree.clyde.domain.model._Role;
 
 public class UserDTO {
+
     private Long id;
     private String name;
     private String email;
